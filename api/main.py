0f9b19b5a99632f01a2d3549337ed7d69d1bb485
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-06-13 11:35:20 -0700

    Add api and frontend boilerplate

---
+++ api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read():
+    return { "Hello": "World" }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
