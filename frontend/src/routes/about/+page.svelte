8c0690894aa8b09f8e18e5d2ed376cd8d2714193
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-06-21 12:55:54 -0700

    Update blog intro

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -11,21 +11,32 @@
 	</div>
 
 	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque labore eligendi quis consequatur
-		impedit nostrum tempore, ex modi odio unde earum ad delectus quisquam similique eos provident
-		quos ullam odit!
+		<b><i>Predict My Tech Salary</i></b> focuses on the impact that demographic data has on the
+		compensation of employees in the technical industry. The goal of this project is to develop a
+		model that predicts a yearly salary given arbitrary parameters, such as ethnicity, gender, age,
+		education level, and accessibility. This data science project is a part of the
+		<b>Project ACCESS</b> Summer 2024 Research Program with the overarching goal of raising awareness
+		of emerging social justice issues in STEM.
+	</p>
+
+	<h2>Exploring data sets</h2>
+
+	<p>
+		We began by first exploring which data set to use for our model. We wanted the data to include
+		information such as the total yearly compensation for a tech employee and demographic data about
+		those individuals. For our purposes, the <b>Stack Overflow 2022 Developer Survey </b> was the most
+		suitable as it contained information about total converted compensation for developers around the
+		world, including self-reported demographic data, such as gender, age, and ethnicity.
 	</p>
 
 	<h2>Building the model</h2>
-
-	<h2>Challenges with building the model</h2>
 
 	<h2>The website</h2>
 
 	<p>
 		We wanted the user to easily interact with our model, and our best idea was to create a web user
-		interface with user input and a form submission button. With those things in mind, we decided to
-		build this web application using <b>SvelteKit</b> and <b>FastAPI</b>.
+		interface with user input and a form submission button. For our purposes, we built this web
+		application using <b>SvelteKit</b> and <b>FastAPI</b>.
 	</p>
 	<p>
 		SvelteKit is a framework for building robust and performant web applications using Svelte, the
@@ -41,13 +52,15 @@
 		request to the API endpoint (the FastAPI application). If all goes well, the endpoint will
 		respond with the predicted salary and render it in the website.
 	</p>
-
 	<img src="/assets/client-server.png" alt="Client and Server interaction for our Website" />
-
 	<p>
 		Thankfully, this is quite a standard client-server interaction, so we did not encounter many
 		issues implementing this part of the project.
 	</p>
+
+	<h2>Challenges with building the model</h2>
+
+	<p>We had a shiton of problems.</p>
 
 	<div class="tags">
 		{#each tags as tag}
